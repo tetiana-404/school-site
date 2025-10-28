8fa242448b25c1e2330e6936431627ccd046cb3c
tetiana-404 <tsamolyuk@gmail.com>
2025-10-29 01:02:14 +0200

    server.js change

--- server/server.js
+++ server/server.js
@@ -2303,7 +2303,11 @@ app.put("/api/contact", async (req, res) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+/*const PORT = process.env.PORT || 5000;
 sequelize.sync().then(() => {
   app.listen(PORT, () => console.log(`🚀 Server running on http://localhost:${PORT}`));
-});
+});*/
+
+sequelize.sync();
+
+module.exports = app;
